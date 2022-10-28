38fdc635135dd737ed7622cc3a938047512f9ab8
akruhoberec1 <50674565+akruhoberec1@users.noreply.github.com>
2022-10-28 14:56:23 +0200

    Ne radi stranicenje
    uploadano za nastavak rada, main zadnja verzija bez errora

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$lifetime=600;
 session_start();
+setcookie(session_name(),session_id(),time() + $lifetime);
+
 
 define('BP',__DIR__ . DIRECTORY_SEPARATOR);
 define('BP_APP', BP . 'app' . DIRECTORY_SEPARATOR);
@@ -38,3 +41,5 @@ spl_autoload_register(function($klasa){
 });
 
 App::start();
+
+
